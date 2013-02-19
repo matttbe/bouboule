b396266c3b453fc54ea1ae85d251c77fe0faa30a
hg <devnull@localhost>
2013-02-19 12:05:21 +0100

    Add missing files

---
+++ Bouboule-android/src/com/me/mytest/MainActivity.java
@@ -0,0 +1,22 @@
+package com.me.mytest;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+public class MainActivity extends AndroidApplication {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
+        //cfg.useGL20 = false;
+        
+        cfg.useGL20 = true;
+        cfg.useAccelerometer = false;
+        cfg.useCompass = false;
+        
+        initialize(new Drop3(), cfg);
+    }
+}
