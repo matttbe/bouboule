f3b829cdc71999b5956dad39b4418d4fffbe67e9
hg <devnull@localhost>
2013-02-19 12:04:01 +0100

    Update + Test Show

--- Bouboule-android/src/be/ac/ucl/lfsab1509/bouboule/MainActivity.java
+++ Bouboule-android/src/be/ac/ucl/lfsab1509/bouboule/MainActivity.java
@@ -1,6 +1,7 @@
 package be.ac.ucl.lfsab1509.bouboule;
 
 import android.os.Bundle;
+import com.me.mytest.*;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
@@ -11,8 +12,12 @@ public class MainActivity extends AndroidApplication {
         super.onCreate(savedInstanceState);
         
         AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
-        cfg.useGL20 = false;
+        //cfg.useGL20 = false;
         
-        initialize(new GdxLib(), cfg);
+        cfg.useGL20 = true;
+        cfg.useAccelerometer = false;
+        cfg.useCompass = false;
+        
+        initialize(new Drop3(), cfg);
     }
 }
