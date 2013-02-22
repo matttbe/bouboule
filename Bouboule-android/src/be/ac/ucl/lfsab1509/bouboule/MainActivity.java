0838117a240affd3939864e2bfbfa97f692e15a8
Nicolas Van Wallendael <nicolas.vanwallendael@student.uclouvain.be>
2013-02-22 16:53:42 +0100

    Package update + activity update

--- Bouboule-android/src/be/ac/ucl/lfsab1509/bouboule/MainActivity.java
+++ Bouboule-android/src/be/ac/ucl/lfsab1509/bouboule/MainActivity.java
@@ -1,8 +1,7 @@
 package be.ac.ucl.lfsab1509.bouboule;
 
 import android.os.Bundle;
-import com.me.mytest.*;
-
+import be.ac.ucl.lfsab1508.bouboule.game.Drop3;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
