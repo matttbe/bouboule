06fc6ef5510551b2f09de3476c6f2afb841aa984
Nicolas Van Wallendael <nicolas.vanwallendael@student.uclouvain.be>
2013-02-26 00:36:11 +0100

    Game image update

--- Bouboule-android/src/be/ac/ucl/lfsab1509/bouboule/game/Game.java
+++ Bouboule-android/src/be/ac/ucl/lfsab1509/bouboule/game/Game.java
@@ -1,7 +1,6 @@
 package be.ac.ucl.lfsab1509.bouboule.game;
 
 import android.util.Log;
-//import be.ac.ucl.lfsab1509.bouboule.R;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
@@ -15,33 +14,37 @@ public class Game implements ApplicationListener {
 
 	final static int appWidth = 480;//Gdx.graphics.getWidth();
 	final static int appHeigth = 800;//Gdx.graphics.getHeight();
-	
-	Texture				boubouleImg;
-	Circle 				bouboule;
-	SpriteBatch 		batch;
-	OrthographicCamera 	camera;
+
+	Texture                         boubouleImg;
+	Texture                         plateauImg;
+	Circle                          bouboule;
+	Circle                          plateau;
+	SpriteBatch             batch;
+	OrthographicCamera      camera;
+
 
 	@Override
 	public void create() {
 
-		boubouleImg = new Texture(Gdx.files.internal("images/bouboule.png"));
+		boubouleImg = new Texture(Gdx.files.internal("images/boub.png"));
+		plateauImg = new Texture(Gdx.files.internal("images/plateau.png"));
 
 		// create the camera and the SpriteBatch
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, appWidth, appHeigth);
-		
+
 		//Batch
 		batch = new SpriteBatch();
 
-		
+
 		// create a Circle to logically represent Bouboule
-		
-		 bouboule = new Circle(appWidth/2-40/2, 60, 64/2);
-		 
-		 
-		 
-		
-		
+
+		plateau = new Circle(0 + appWidth/2 , (appHeigth-appWidth) /2 + appWidth/2, appWidth/2);
+		bouboule = new Circle(plateau.x, plateau.y, 64/2);
+
+
+
+
 	}
 
 
@@ -49,55 +52,62 @@ public class Game implements ApplicationListener {
 
 	@Override
 	public void render() {
-		
+
 		// clear the screen with a dark blue color. The
-	      // arguments to glClearColor are the red, green
-	      // blue and alpha component in the range [0,1]
-	      // of the color to be used to clear the screen.
-	      Gdx.gl.glClearColor(0, 0, 0.2f, 1);
-	      Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-	      
-	      // tell the camera to update its matrices.
-	      camera.update();
-	      
-	      // tell the SpriteBatch to render in the
-	      // coordinate system specified by the camera.
-	      batch.setProjectionMatrix(camera.combined);
-	      
-	      // begin a new batch and draw Bouboule and
-	      batch.begin();
-	      batch.draw(boubouleImg, bouboule.x, bouboule.y);
-	      batch.end();
-	      
-	     
-	      
-	      //Movement 
-	      
-	      //EN X
-	      float accelX = Gdx.input.getAccelerometerX();
-	      float accelY = Gdx.input.getAccelerometerY();
-	      float accelZ = Gdx.input.getAccelerometerZ();
-	      
-	      Log.d("Accelerometer", accelX+"  "+accelY+"  "+accelZ+"  ");
-	      
-	      if(accelX >  0 ) bouboule.x -= 200 * Gdx.graphics.getDeltaTime();
-	      if(accelX <= 0 ) bouboule.x += 200 * Gdx.graphics.getDeltaTime();
-	      
-	      // make sure the bucket stays within the screen bounds
-	      if(bouboule.x < 0) bouboule.x = 0;
-	      if(bouboule.x > (appWidth - bouboule.radius) ) bouboule.x = appWidth - bouboule.radius;
-	      
-	      
-	      
-	      //EN Y
-	      if(accelY >  0 ) bouboule.y -= 200 * Gdx.graphics.getDeltaTime();
-	      if(accelY <= 0 ) bouboule.y += 200 * Gdx.graphics.getDeltaTime();
-	      
-	      // make sure the bucket stays within the screen bounds
-	      if(bouboule.y < 0) bouboule.y = 0;
-	      if(bouboule.y > (appHeigth - bouboule.radius) ) bouboule.y = appHeigth - bouboule.radius ;
-	      
-	      
+		// arguments to glClearColor are the red, green
+		// blue and alpha component in the range [0,1]
+		// of the color to be used to clear the screen.
+		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		// tell the camera to update its matrices.
+		camera.update();
+
+		// tell the SpriteBatch to render in the
+		// coordinate system specified by the camera.
+		batch.setProjectionMatrix(camera.combined);
+		// begin a new batch and draw Bouboule and
+		batch.begin();
+		batch.draw(plateauImg, plateau.x - plateau.radius, plateau.y - plateau.radius, plateau.radius*2 , plateau.radius*2 ); 
+		batch.draw(boubouleImg, bouboule.x - bouboule.radius, bouboule.y - bouboule.radius);
+		batch.end();
+
+
+
+		//Mouvement 
+
+		//EN X
+		float accelX = Gdx.input.getAccelerometerX();
+		float accelY = Gdx.input.getAccelerometerY();
+		float accelZ = Gdx.input.getAccelerometerZ();
+
+		Log.d("Accelerometer", accelX+"  "+accelY+"  "+accelZ+"  ");
+
+		//move en X
+		if(accelX >  0 ) bouboule.x -= 150 * Gdx.graphics.getDeltaTime();
+		if(accelX <= 0 ) bouboule.x += 150 * Gdx.graphics.getDeltaTime();
+
+		//move EN Y
+		if(accelY >  0 ) bouboule.y -= 150 * Gdx.graphics.getDeltaTime();
+		if(accelY <= 0 ) bouboule.y += 150 * Gdx.graphics.getDeltaTime();
+
+
+		//remetre la boule au centre quand elle tombe hors du ring
+		if((bouboule.x-plateau.x)*(bouboule.x-plateau.x) + (bouboule.y-plateau.y)*(bouboule.y-plateau.y) >= plateau.radius*plateau.radius){
+			bouboule.x=plateau.x;
+			bouboule.y=plateau.y;
+		}
+
+		/*
+              // make sure the bouboule stays within the screen bounds
+              if(bouboule.x < 0) bouboule.x = 0;
+              if(bouboule.x > (appWidth - bouboule.radius) ) bouboule.x = appWidth - bouboule.radius;
+
+              // make sure the bouboule stays within the screen bounds
+              if(bouboule.y < 0) bouboule.y = 0;
+              if(bouboule.y > (appHeigth - bouboule.radius) ) bouboule.y = appHeigth - bouboule.radius ;
+		 */
+
 	}
 
 
@@ -105,10 +115,11 @@ public class Game implements ApplicationListener {
 
 	@Override
 	public void dispose() {
-		
+
 		boubouleImg.dispose();
+		plateauImg.dispose();
 		batch.dispose();
-		
+
 	}
 
 	@Override
@@ -123,5 +134,3 @@ public class Game implements ApplicationListener {
 	public void resume() {
 	}
 }
-
-
