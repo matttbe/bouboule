e38598f102b6da2771fec5341834831fd0f3654d
Baptiste Remy <baptiste.remy@student.uclouvain.be>
2013-04-10 14:44:41 +0200

    Upgrade IA

--- Bouboule/src/be/ac/ucl/lfsab1509/bouboule/game/ia/IA.java
+++ Bouboule/src/be/ac/ucl/lfsab1509/bouboule/game/ia/IA.java
@@ -1,32 +1,6 @@
 package be.ac.ucl.lfsab1509.bouboule.game.ia;
 
-/*
- * This file is part of Bouboule.
- * 
- * Copyright 2013 UCLouvain
- * 
- * Authors:
- *  * Group 7 - Course: http://www.uclouvain.be/en-cours-2013-lfsab1509.html
- *    Matthieu Baerts <matthieu.baerts@student.uclouvain.be>
- *    Baptiste Remy <baptiste.remy@student.uclouvain.be>
- *    Nicolas Van Wallendael <nicolas.vanwallendael@student.uclouvain.be>
- *    Helene Verhaeghe <helene.verhaeghe@student.uclouvain.be>
- * 
- * Bouboule is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-
-
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import be.ac.ucl.lfsab1509.bouboule.game.body.Bouboule;
@@ -82,9 +56,10 @@ public class IA {
 			Acc = gyroscope();
 			//Acc = troll2(LocalEnemi, VelocityEnemi);
 			//Acc = middeler(LocalEnemi,0);
-			//Acc = aggretion(LocalEnemi, VelocityEnemi,IA);
-			//Acc = hybrid(LocalEnemi,VelocityEnemi,IA);
+			//Acc = aggretion(LocalEnemi, VelocityEnemi,IA,VelocityIA);
+			//Acc = hybrid(LocalEnemi,VelocityEnemi,IA,VelocityIA);
 			//Acc = stopMid(LocalEnemi, VelocityEnemi);
+			//Gdx.app.log ("Player","player"+LocalEnemi.x+"speed"+VelocityEnemi.x+"acc"+Acc.x);
 			break;
 
 		case 1:
@@ -98,13 +73,13 @@ public class IA {
 			Acc = stopMid(IA, VelocityIA);
 			break;
 		case 4:
-			Acc = aggretion(IA,VelocityIA,LocalEnemi);
+			Acc = aggretion(IA,VelocityIA,LocalEnemi,VelocityEnemi);
 			break;
 		case 5:
 			Acc = defence(IA,VelocityIA,LocalEnemi);
 			break;
 		case 6:
-			Acc = hybrid(IA,VelocityIA,LocalEnemi);
+			Acc = hybrid(IA,VelocityIA,LocalEnemi,VelocityEnemi);
 			break;
 		case 7:
 			break;
@@ -123,15 +98,17 @@ public class IA {
 			slow = new Vector2(VelocityEnemi).nor().mul(0.02f);
 			//slow = new Vector2(0, 0);
 		}
-		Acc.sub(slow);
+		//Acc.sub(slow);
 		
-
+		if(IALevel == 0)
+		Gdx.app.log ("Player","player"+LocalEnemi.x+"speed"+VelocityEnemi.x+"acc"+Acc.x);
+		
 		return Acc;
 	}
 
 
-	private static Vector2 hybrid(Vector2 IA, Vector2 velocityIA,Vector2 localEnemi) {
-		float anglerelatif = angleCentre(IA, 0) - angleCentre(localEnemi, 0);
+	private static Vector2 hybrid(Vector2 IA, Vector2 velocityIA,Vector2 localEnemi,Vector2 VelocityEnemi) {
+		float anglerelatif = angleCentre(IA, getClosestCentre(IA).getVector()) - angleCentre(localEnemi, getClosestCentre(IA).getVector());
 		anglerelatif=casteangle(anglerelatif);
 		float distIA,distEnemi;
 		distIA = GlobalSettings.ARENAWAYPOINTALLOW.get(0).getVector().dst(IA);
@@ -140,16 +117,18 @@ public class IA {
 		if(anglerelatif < 45 && anglerelatif > -45 && distIA>distEnemi){
 			return defence(IA, velocityIA, localEnemi);
 		}else{
-			return aggretion(IA, velocityIA, localEnemi);
+			return aggretion(IA, velocityIA, localEnemi, VelocityEnemi);
 		}
 
 	}
 
 
 	private static Vector2 defence(Vector2 IA, Vector2 velocityIA,Vector2 localEnemi) {
-		Vector2 acc = new Vector2(middeler(IA, 0)).nor();
-		float angleEnemi = angleCentre(localEnemi, 0) - angleCentre(IA, 0);
-		float direction = angleCentre(IA, 0) - velocityIA.angle();
+		MapNode close =getClosestCentre(IA);
+		Vector2 vClose = close.getVector();
+		Vector2 acc = new Vector2(middeler(IA, close)).nor();
+		float angleEnemi = angleCentre(localEnemi, vClose) - angleCentre(IA, vClose);
+		float direction = angleCentre(IA, vClose) - velocityIA.angle();
 
 		angleEnemi = casteangle(angleEnemi);
 		direction = casteangle(direction);
@@ -159,27 +138,34 @@ public class IA {
 			acc.rotate(-75);
 		}
 
-		Vector2 dirmid = middeler(IA, 0);
-		if(dirmid.dot(velocityIA) < 0.25f && velocityIA.len2()+dirmid.len()*8 > 10){
+		Vector2 dirmid = middeler(IA, close);
+		if(dirmid.dot(velocityIA) < 0.25f && velocityIA.len2()+dirmid.len() > close.getWeight()){
 			//évitement des bords
+			Gdx.app.log ("Player","IA:defence slow");
 			return stopMid(IA, velocityIA);
 		}
-
+		Gdx.app.log ("Player","IA:defence normal");
 		return acc;
 	}
 
 
-	private static Vector2 aggretion(Vector2 position, Vector2 velocity,Vector2 localEnemi) {
-		Vector2 directionenemi , dirmid;
-		dirmid = middeler(position, 0);
+	private static Vector2 aggretion(Vector2 position, Vector2 velocity,Vector2 localEnemi, Vector2 VelocityEnemi) {
+		Vector2 directionenemi , dirmid , fictposition;
+		MapNode centreIA = getClosestCentre(position);
+		dirmid = middeler(position, centreIA);
 		//dirmid.nor();
+		fictposition = new Vector2(position);
+		fictposition.add(new Vector2(velocity).mul(2));
 		directionenemi = new Vector2(localEnemi);
-		directionenemi.sub(position).nor();
-		if(dirmid.dot(velocity) < 0.15f && velocity.len2()+dirmid.len()*8 > 30){
-
+		directionenemi.add(new Vector2(VelocityEnemi.mul(2))).sub(fictposition).nor();
+		
+		if(!(Math.abs(angleCentre(fictposition, centreIA.getVector())-angleCentre(localEnemi,centreIA.getVector())) < 10))
+		if(dirmid.dot(velocity) < 0.0f && velocity.len2()+dirmid.len() > centreIA.getWeight()){
+			Gdx.app.log ("Player","IA:attaque slow");
 			//évitement des bords
 			return stopMid(position, velocity);
 		}
+		Gdx.app.log ("Player","IA:attaque normal");
 		return directionenemi;
 	}
 
@@ -193,16 +179,23 @@ public class IA {
 		 */
 		//accelX=0;
 		//accelY=0;
-		// Gdx.app.log ("IA","gyroscope"+ Acc.len());
+		//Gdx.app.log ("IA","gyroscope"+ Acc.len());
 
 		return Acc;
 	}
 
-	public static Vector2 middeler(Vector2 IA,int temp){
+	public static Vector2 middeler(Vector2 IA,int centre){
 		
-		Vector2 Acc = new Vector2(GlobalSettings.ARENAWAYPOINTALLOW.get(temp).getVector());
+		Vector2 Acc = new Vector2(GlobalSettings.ARENAWAYPOINTALLOW.get(centre).getVector());
 		Acc.sub(IA);
 		
+		return Acc;
+	}
+	
+	public static Vector2 middeler(Vector2 IA,MapNode centre){
+		
+		Vector2 Acc = new Vector2(centre.getVector());
+		Acc.sub(IA);
 		
 		return Acc;
 	}
@@ -216,7 +209,7 @@ public class IA {
 		Acc = new Vector2(dirmid).sub(vitesse).nor();
 
 		if(dirmid.dot(vitesse) > 0 &&
-				GlobalSettings.ARENAWAYPOINTALLOW.get(0).getVector().dst(position) < vitesse.dot(dirmid)*vitesse.dot(dirmid)/(2*GlobalSettings.LIMITACC)){
+				getClosestCentre(position).getVector().dst(position) < vitesse.dot(dirmid)*vitesse.dot(dirmid)/(2*GlobalSettings.LIMITACC)){
 			Acc.rotate(180);
 		}else{
 		}
@@ -285,9 +278,9 @@ public class IA {
 	/*
 	 * renvoi un angle entre ]-180;180[
 	 */
-	private static float angleCentre(Vector2 position,int centre){
+	private static float angleCentre(Vector2 position,Vector2 centre){
 		Vector2 temp = new Vector2(position);
-		temp.sub(GlobalSettings.ARENAWAYPOINTALLOW.get(centre).getVector());
+		temp.sub(centre);
 		return casteangle(temp.angle());
 	}
 
@@ -296,6 +289,21 @@ public class IA {
 	 */
 	private static float casteangle(float angle){
 		return ((angle + 180)% 360 - 180);
+	}
+	
+	private static MapNode getClosestCentre(Vector2 position){
+		ArrayList<MapNode> tabNode = GlobalSettings.ARENAWAYPOINTALLOW;
+		float mindist = position.dst(tabNode.get(0).getVector());
+		int min = 0;
+		float tempdist;
+		for(int i=1;i<tabNode.size();i++){
+			tempdist=position.dst(tabNode.get(i).getVector());
+			if(tempdist < mindist){
+				mindist = tempdist;
+				min = i;
+			}
+		}
+		return tabNode.get(min);
 	}
 
 }
