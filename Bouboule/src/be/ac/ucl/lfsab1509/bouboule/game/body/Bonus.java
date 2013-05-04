8cecb927c0fe1e873d92f653a22cabcf4b6da163
Baptiste Remy <baptiste.remy@student.uclouvain.be>
2013-05-04 23:11:51 +0200

    Ajout de bonus au millieu des waypoints

--- Bouboule/src/be/ac/ucl/lfsab1509/bouboule/game/body/Bonus.java
+++ Bouboule/src/be/ac/ucl/lfsab1509/bouboule/game/body/Bonus.java
@@ -75,7 +75,7 @@ public class Bonus extends GameBody{
 		MapNode node = GlobalSettings.ARENAWAYPOINTALLOW.get(random.nextInt(size));
 
 		Vector2 pos	= new Vector2(node.xToPixel()-32, node.yToPixel()-32);
-		Vector2 radius = new Vector2(node.weightToPixel(),0);
+		Vector2 radius = new Vector2(node.weightToPixel()*random.nextFloat(),0);
 		radius.rotate(random.nextInt(359));
 		pos.add(radius);
 
