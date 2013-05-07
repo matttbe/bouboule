b6d7874340759113b5a2d2b5d514c8792684dbce
Matthieu Baerts <matthieu.baerts@student.uclouvain.be>
2013-05-07 13:02:01 +0200

    ScreenGame: when changing the loopMusic, dispose it before (fixed memory leaks)

--- Bouboule/src/be/ac/ucl/lfsab1509/bouboule/game/screen/ScreenGame.java
+++ Bouboule/src/be/ac/ucl/lfsab1509/bouboule/game/screen/ScreenGame.java
@@ -167,6 +167,7 @@ public class ScreenGame implements Screen {
 
 		boolean bWasPlaying = loopMusic.isPlaying();
 		loopMusic.stop();
+		loopMusic.dispose();
 
 		loopMusicFile = pNewMusicFile;
 		loopMusic = Gdx.audio.newMusic(pNewMusicFile);
