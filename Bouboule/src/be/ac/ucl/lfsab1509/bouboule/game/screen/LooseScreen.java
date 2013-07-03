5a5d5e591775b043101ce84827e2754962d2ff52
Nicolas Van Wallendael <nicolas.vanwallendael@student.uclouvain.be>
2013-07-03 16:00:46 +0200

    [Add] Win/Loose Game Screen - wait for @math to set up.

---
+++ Bouboule/src/be/ac/ucl/lfsab1509/bouboule/game/screen/LooseScreen.java
@@ -0,0 +1,81 @@
+package be.ac.ucl.lfsab1509.bouboule.game.screen;
+
+/*
+ * This file is part of Bouboule.
+ * 
+ * Copyright 2013 UCLouvain
+ * 
+ * Authors:
+ *  * Group 7 - Course: http://www.uclouvain.be/en-cours-2013-lfsab1509.html
+ *    Matthieu Baerts <matthieu.baerts@student.uclouvain.be>
+ *    Baptiste Remy <baptiste.remy@student.uclouvain.be>
+ *    Nicolas Van Wallendael <nicolas.vanwallendael@student.uclouvain.be>
+ *    H??l??ne Verhaeghe <helene.verhaeghe@student.uclouvain.be>
+ * 
+ * Bouboule is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import be.ac.ucl.lfsab1509.bouboule.game.gameManager.GlobalSettings;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+
+
+public class LooseScreen extends AbstractScreen {
+	
+
+	public LooseScreen(boolean bMusicNeedsDelay) {
+		super(bMusicNeedsDelay);
+	}
+
+	@Override
+	public void show() {
+		super.show();
+
+		//Set Background
+		
+		addBackGround("drawable-xhdpi/you_lose.jpg");
+		
+		//Create all Buttons - Play Button
+		
+		Button playButton  = createButton("transparent", 430, 160, 200, 725);
+		Button paramButton = createButton("transparent", 430, 160, 200, 555);
+		Button scoreButton = createButton("transparent", 430, 160, 200, 385);
+		
+		
+		playButton.addListener( new ClickListener() {
+			public void clicked (InputEvent event, float x, float y) {
+				Gdx.app.log ("SCREEN", "clickStart " + x + ", " + y);
+				GlobalSettings.GAME.setScreenGame();
+			}
+		});
+		
+		paramButton.addListener( new ClickListener() {
+			public void clicked (InputEvent event, float x, float y) {
+				Gdx.app.log ("SCREEN", "clickParam " + x + ", " + y);
+				//TODO: setScreen(new ParamScreen());
+			}
+		});
+		
+		scoreButton.addListener( new ClickListener() {
+			public void clicked (InputEvent event, float x, float y) {
+				Gdx.app.log ("SCREEN", "clickScore " + x + ", " + y);
+				//TODO: setScreen(new HighScreen()); // or something else
+			}
+		});
+
+	}
+}
