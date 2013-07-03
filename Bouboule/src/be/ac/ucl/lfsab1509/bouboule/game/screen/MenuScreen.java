daa07a37ba18cfc66a703f18511e83ccfa6b8313
Matthieu Baerts <matthieu.baerts@student.uclouvain.be>
2013-07-03 12:28:31 +0200

    screen: added missing licence

--- Bouboule/src/be/ac/ucl/lfsab1509/bouboule/game/screen/MenuScreen.java
+++ Bouboule/src/be/ac/ucl/lfsab1509/bouboule/game/screen/MenuScreen.java
@@ -1,5 +1,31 @@
 package be.ac.ucl.lfsab1509.bouboule.game.screen;
 
+/*
+ * This file is part of Bouboule.
+ * 
+ * Copyright 2013 UCLouvain
+ * 
+ * Authors:
+ *  * Group 7 - Course: http://www.uclouvain.be/en-cours-2013-lfsab1509.html
+ *    Matthieu Baerts <matthieu.baerts@student.uclouvain.be>
+ *    Baptiste Remy <baptiste.remy@student.uclouvain.be>
+ *    Nicolas Van Wallendael <nicolas.vanwallendael@student.uclouvain.be>
+ *    Hélène Verhaeghe <helene.verhaeghe@student.uclouvain.be>
+ * 
+ * Bouboule is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
@@ -22,9 +48,9 @@ public class MenuScreen extends AbstractScreen {
 		
 		//Create all Buttons - Play Button
 		
-		Button playButton 		= createButton("transparent", 430, 160, 200, 725);
-		Button paramButton 		= createButton("transparent", 430, 160, 200, 555);
-		Button scoreButton 	= createButton("transparent", 430, 160, 200, 385);
+		Button playButton  = createButton("transparent", 430, 160, 200, 725);
+		Button paramButton = createButton("transparent", 430, 160, 200, 555);
+		Button scoreButton = createButton("transparent", 430, 160, 200, 385);
 		
 		
 		playButton.addListener( new ClickListener() {
