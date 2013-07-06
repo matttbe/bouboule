af3b1762096608c40ccec8846bb95dc52413fc4b
Matthieu Baerts <matthieu.baerts@student.uclouvain.be>
2013-07-06 13:56:20 +0200

    GdxMenus: About: added a few more info

--- Bouboule/src/be/ac/ucl/lfsab1509/bouboule/game/screen/AboutScreen.java
+++ Bouboule/src/be/ac/ucl/lfsab1509/bouboule/game/screen/AboutScreen.java
@@ -2,6 +2,7 @@ package be.ac.ucl.lfsab1509.bouboule.game.screen;
 
 import be.ac.ucl.lfsab1509.bouboule.game.gameManager.GlobalSettings;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 
@@ -15,8 +16,30 @@ public class AboutScreen extends AbstractScreen {
 	public void show() {
 		super.show();
 
-		Label label = new Label("Ceci est un super test :)\nSur 2 lignes\net un super super super super super super super super super super super super super super super super super super super long text", getSkin(), "default", Color.WHITE);
-		//label.setFontScale(.5f);
+		// Note: no accent with the default font and multiple spaces or \t are not working
+		String cAboutText =
+				  "App's name:\n"
+				+ " Bouboule\n\n"
+				+ "Authors:\n"
+				+ " Baerts Matthieu, Remy Baptiste, Van Wallendael "
+					+ "Nicolas and Verhaeghe Helene\n\n"
+				+ "Graphisms:\n"
+				+ " Van Wallendael Julien\n\n"
+				+ "Music and sounds:\n"
+				+ " Adans Paschal (adanspaschal@gmail.com) and"
+					+ " Klez (The gateway - SpaceShip level)\n\n"
+				+ "Credits & License:\n"
+				+ " Bouboule was developed during the course"
+					+ " FSAB1509 - Projet Informatique (Yves Deville, Universite"
+					+ " Catholique de Louvain, Belgium), 2012–2013.\nOpen-Source"
+					+ " GPL3 license and developed with LibGDX (Free and"
+					+ " Open-Source game development framework under Apache"
+					+ " License 2.0).";
+		int iVersion = Gdx.app.getVersion();
+		if (iVersion > 0)
+			cAboutText += "\n\nVersion: " + iVersion;
+		Label label = new Label(cAboutText, getSkin(), "default", Color.WHITE);
+		// label.setFontScale(.5f);
 		label.setWidth(GlobalSettings.APPWIDTH - 20);
 		label.setHeight(GlobalSettings.APPHEIGHT - 20);
 		label.setPosition(10, 10);
@@ -26,7 +49,6 @@ public class AboutScreen extends AbstractScreen {
 		//Set Background
 		addBackGround("drawable-xhdpi/settings_blank.jpg");
 
-		// addLabel("Ceci est un super test :)\nSur 2 lignes", "oswald-font", .5f, Color.WHITE, 10, 10);
 		stage.addActor(label);
 
 		//Create all Buttons
