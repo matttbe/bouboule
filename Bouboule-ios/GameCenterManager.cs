55572378a7bd350fd525b2f0db2fc96e800c79d8
Nicolas Van Wallendael <nicolas.vanwallendael@student.uclouvain.be>
2013-08-11 17:09:11 +0200

    [Update] License

--- Bouboule-ios/GameCenterManager.cs
+++ Bouboule-ios/GameCenterManager.cs
@@ -8,7 +8,33 @@ using com.badlogic.gdx.backends.ios;
 using com.badlogic.gdx;
 using be.ac.ucl.lfsab1509.bouboule.game;
 
-//Based on Xamarin Documentation and 
+//Based on Xamarin Documentation and Trasc0 example from libGDX forum
+
+/*
+ * This file is part of Bouboule.
+ * 
+ * Copyright 2013 UCLouvain
+ * 
+ * Authors:
+ *  * Group 7 - Course: http://www.uclouvain.be/en-cours-2013-lfsab1509.html
+ *    Matthieu Baerts <matthieu.baerts@student.uclouvain.be>
+ *    Baptiste Remy <baptiste.remy@student.uclouvain.be>
+ *    Nicolas Van Wallendael <nicolas.vanwallendael@student.uclouvain.be>
+ *    Hélène Verhaeghe <helene.verhaeghe@student.uclouvain.be>
+ * 
+ * Bouboule is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 namespace GameCenterIOS
 {
